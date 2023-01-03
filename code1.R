ef874b6d2e1bbb971cec8c50677fa23c65648f15
Senushia1 <senushia1@gmail.com>
2023-01-03 18:33:29 +0800

    2nd Commit

--- code1.R
+++ code1.R
@@ -6,6 +6,7 @@ library(shinythemes)
 library(readxl)
 library(tidyr)
 
+#ui
 ui <- fluidPage(
   navbarPage( title = "Cumulative Claims",
               position = "static-top",
@@ -23,7 +24,7 @@ ui <- fluidPage(
   ),
 )
 
-
+#server
 server <- function(input,output) {
   
   DF  <- reactive({
@@ -47,14 +48,11 @@ server <- function(input,output) {
       }
     }
     
-    DFWideC
     DFWideC$Development_Year_4 <- rev(DFWideC)[1] * input$tail
-    DFWideC
     colnames(DFWideC) <- c("Development_Year_1", "Development_Year_2", "Development_Year_3", "Development_Year_4" )
-    k <- cbind(DFWide[1],DFWideC[])
-    colnames(k)[1] <- "Loss Year"
-    p <- as.data.frame(k)
-    p
+    table_ <- cbind(DFWide[1],DFWideC[])
+    colnames(table_)[1] <- "Loss Year"
+    table_
     
   })
   
@@ -63,18 +61,29 @@ server <- function(input,output) {
     
   })
   
+  data1 <- reactive ({
+    
+    NewDF <- cbind(gather(table_[2:4], key = "Development_Year", value = "Cumulative", factor_key = T ), p[1])
+    NewDF
+    
+  })
+  
   output$scatterplot <- renderPlot({
     
-    t = p %>% pivot_longer(cols = c(p$Loss_Year), names_to = "Loss_Year", values_to = "Cumulative_Loss_Claims")
-    ggplot(data = t, mapping = aes(x = t$Development_Year, y = Cumulative_Loss_Claims, colour = t$Cumulative_Loss_Claims, group = t$Cumulative_Loss_Claims)) +
-      geom_point(mapping = aes(x = Development_Year, y = Cumulative_Loss_Claims)) +
-      
-      labs(x = "Development Year", y = "Cumulative Loss Claims")
+    ggplot(data = data1(), mapping = aes(x = Development_Year, y = Cumulative, colour = NewDF$`Loss Year`, group = NewDF$`Loss Year`), theme = chartTheme("white"), title(main = "Graph of Cumulative Loss Claims"), xlab = "Development_Year", ylab = "Cumulative Loss CLaims") +
+      #geom_smooth(mapping = aes(x = NewDF$Development_Year, y = NewDF$Cumulative)) +
+      geom_smooth() +
+      geom_point(mapping = aes(x = NewDF$Development_Year, y = NewDF$Cumulative))  + labs(title = "Graph of Cumulative Loss Claims", x= "Development Year", y = "Cumulative Loss Claims") 
+    #theme(plot.title = element_text(hjust = 0.5), axis.text.x=element_text(color = "slategray4", size=8, angle=90, vjust=.8, hjust=0.8), plot.background = element_rect(fill = "floralwhite"), panel.background = element_rect(fill = "floralwhite", colour = "black"), 
+    #panel.border = element_rect(fill = "transparent",color = 4, size = 2))
+    
+    
     
   })
   
   
 }
 
+#shinyapp
 shinyApp(ui, server)
 
